975c70dcbbf1175a2f035e521a6ca5d3d96ff2ed
suno97 <ssh9234@gmail.com>
2022-12-12 22:19:01 +0900

    antd4to5-menu-error-fix

--- React/React_nodebird/prepare/front/components/AppLayout.js
+++ React/React_nodebird/prepare/front/components/AppLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { number } from 'prop-types';
 import Link from 'next/link';
 import { Input, Menu, Row, Col } from 'antd';
 import styled, { createGlobalStyle } from 'styled-components';
@@ -26,24 +26,28 @@ const Global = createGlobalStyle`
 `
 const AppLayout = ({ children }) => {
     const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-
+    const menuItems = [
+        {
+            label:<Link href="/"><a>노드버드</a></Link>,
+            key : "nb",
+        },
+        {
+            label:<Link href="/profile"><a>프로필</a></Link>,
+            key : "pf",
+        },
+        {
+            label:<SearchInput enterButton></SearchInput>,
+            key : "si",
+        },
+        {
+            label:<Link href="/signup"><a>회원가입</a></Link>,
+            key : "sign",
+        },
+    ]
     return (
         <div>
             <Global></Global>
-            <Menu mode='horizontal'>
-                <Menu.Item>
-                    <Link href="/"><a>노드버드</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/profile"><a>프로필</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <SearchInput enterButton></SearchInput>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/signup"><a>회원가입</a></Link>
-                </Menu.Item>
-            </Menu>
+            <Menu  items={menuItems} mode='horizontal'></Menu>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
                     {isLoggedIn ? <UserProfile ></UserProfile> : <LoginForm></LoginForm>}
