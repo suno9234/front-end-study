8f774342c9ca9600a06adf7ad2e1ea5cc74aba07
suno97 <ssh9234@gmail.com>
2022-12-18 21:01:51 +0900

    backend-start

---
+++ React/React_nodebird/prepare/back/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const postRouter = require('./routes/post');
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send('hello express');
+});
+
+app.get('/api',(req,res)=>{
+  res.send('hello api');
+});
+
+app.use('/post',postRouter);
+
+app.listen(3065,()=>{
+  console.log('서버 실행 중');
+});
